3143380a1266872e0e041fce874d41e5f36eabe3
Ruqouyyah Muhammad <99969515+mruqqie@users.noreply.github.com>
2022-11-27 00:47:51 -0800

    modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,4 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
  * Define Global Variables
  * 
 */
