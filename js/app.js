fcb99d95546859b3d4dbe4d5d12cab47a6f94b78
Ruqouyyah Muhammad <99969515+mruqqie@users.noreply.github.com>
2022-11-26 23:49:11 -0800

    modified:   css/styles.css 	modified:   index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -22,16 +22,32 @@
  * Define Global Variables
  * 
 */
-const navBarList = document.querySelectorAll('section')
-nav = document.getElementById('navbar__list');
+const navBarList = document.querySelectorAll('section');
+const nav = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+nav.addEventListener("click", (event) => {
+    event.preventDefault()
+    const section = document.getElementById(event.target.id.replaceAll("_", ""));
+    console.log(event)
+    section.scrollIntoView({behaviour: "smooth"});
+})
 
-
+function inViewPort(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        // rect.top >= 0 &&
+        rect.left >= 0 &&
+        // In case the browser does not support .innerHeight, 
+        // it falls back to the older document.documentElement.clientheight
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -47,10 +63,12 @@ function navList() {
             `${section.dataset.nav}`
         );
         a.setAttribute('href', `${section.id}`);
+        a.setAttribute('id', `${section.id}_`);
         a.setAttribute('class', 'menu__link');
         a.appendChild(content);
         li.appendChild(a);
-        nav.appendChild(li);
+        nav.append(li);
+       
     });
 
 }
@@ -71,6 +89,11 @@ navList();
 
 // Scroll to section on link click
 
+
 // Set sections as active
-
-
+document.addEventListener("scroll", () => {
+    navBarList.forEach((section) => {
+        inViewPort(section) ? section.classList.add("active") :
+        section.classList.remove("active");
+    });
+})
